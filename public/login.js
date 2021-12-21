09b460de3b8b36457abdc22742a94d826e28c2f6
Carlotta Chen <carlottachen92@gmail.com>
2021-12-20 21:55:08 -0800

    js files for login and signup

---
+++ public/login.js
@@ -0,0 +1,31 @@
+const loginSubmit = document.querySelector('#login-form');
+
+const baseURL = `http://localhost:4004/api`
+
+function loginUser(event) {
+    alert('click');
+
+    let username = document.querySelector('#username-field');
+    let password = document.querySelector('#password-field');
+
+    let loginObj = {
+        username: username.value,
+        password: password.value
+    }
+
+    login(loginObj);
+
+    username.value = '';
+    password.value = '';
+}
+
+function login(body) {
+    axios.post(`${baseURL}/login`, body).then(response => {
+        alert('Signed in!');
+    }).catch(error => {
+        console.log(error);
+        alert('Uh oh. Your request did not work.');
+    })
+}
+
+loginSubmit.addEventListener('submit', loginUser);
---
+++ public/signup.js
@@ -0,0 +1,41 @@
+const signupForm = document.querySelector('#signup-form');
+const message = document.querySelector('#message');
+
+const baseURL = `http://localhost:4004/api`
+
+function registerNewUser(event) {
+    event.preventDefault();
+    let email = document.querySelector('#signup-email-field')
+    let username = document.querySelector('#signup-username-field');
+    let password = document.querySelector('#signup-password-field');
+    let confirmPass = document.querySelector('#confirm-password-field');
+
+    if (password.value != confirmPass.value) {
+        alert('Passwords must match!');
+        return;
+    }
+
+    let newUserObj = {
+        email: email.value,
+        username: username.value,
+        password: password.value
+    }
+
+    register(newUserObj);
+
+    email.value = '';
+    username.value = '';
+    password.value = '';
+    confirmPass.value = '';
+}
+
+function register(body) {
+    axios.post(`${baseURL}/register`, body).then(response => {
+        message.innerHTML = 'Successfully registered new user';
+    }).catch(error => {
+        console.log(error);
+        alert('Uh oh. Your request did not work');
+    })
+}
+
+signupForm.addEventListener('submit', registerNewUser);
