7309ebefd6ba9a435375d5b042f727b664dd2265
Carlotta Chen <carlottachen92@gmail.com>
2021-12-22 13:48:25 -0800

    change port

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const { SERVER_PORT } = process.env;
+//const { SERVER_PORT } = process.env;
 
 
 const {
@@ -23,4 +23,5 @@ app.get('/', function (request, response) {
 app.post(`/api/login`, login);
 app.post(`/api/register`, register);
 
-app.listen(SERVER_PORT, () => console.log(`Welcome to PORT ${SERVER_PORT}`));
+const port = process.env.PORT || 4004;
+app.listen(port, () => console.log(`Welcome to PORT ${port}`));
